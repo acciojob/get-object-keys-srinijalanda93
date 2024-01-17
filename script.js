0601230e95adef4195df1705eda5231258874a46
srinija landa <144719297+srinijalanda93@users.noreply.github.com>
2024-01-17 23:58:26 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
-//your JS code here. If required.
+
+// Creating the student object
+const student = {
+  name: "John Doe"
+};
+
+// Adding getKeys() method to the Object prototype
+Object.prototype.getKeys = function() {
+  return Object.keys(this);
+};
+
+// Testing the getKeys() method on the student object
+const keysArray = student.getKeys();
+console.log(keysArray);
